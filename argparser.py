cf5e74f16356e154956842803878b60ff296cd37
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-02-16 12:20:44 +0800

    Add files via upload

--- argparser.py
+++ argparser.py
@@ -5,29 +5,29 @@ Parser = argparse.ArgumentParser()
 Parser.add_argument(
     "--tags", 
     nargs="+", 
-    help="Tags to filter doujins by", 
+    help="doujin tags to search for", 
     required=True
 )
 Parser.add_argument(
     "--exclude", 
     nargs="+", 
     default=[], 
-    help="Filter out all the nasty shit"
+    help="nasty shit to filter out"
 )
 Parser.add_argument(
     "--page", 
     type=int, 
     default=1, 
-    help="Website page number"
+    help="page number of search results"
 )
 Parser.add_argument(
     "--batchsize", 
     type=int, 
     default=None, 
-    help="Sets the max number of results"
+    help="max number of results"
 )
 Parser.add_argument(
     "--random", 
     action="store_true", 
-    help="Picks a random doujin from the fetched results"
+    help="pick a random doujin from the fetched results"
 )
--- scraper.py
+++ scraper.py
@@ -21,7 +21,7 @@ class Scraper:
                     for i in contents.find_all('a', href=True, class_="gallery-visited-from-favorites")     
                 ][:batch_size]
                 
-                return (choice(doujins) if random else "\n\n".join(doujins)) or "No doujins found"
+                return (choice(doujins or [None]) if random else "\n\n".join(doujins)) or "No doujins found"
             else:
                 return f"{response.status_code}: {response.reason}" 
         
